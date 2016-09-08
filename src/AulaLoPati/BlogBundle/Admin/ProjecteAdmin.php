019c3767303b70a941716802876a587093e4ffd6
David Romaní <david@flux.cat>
2016-09-08 18:58:11 +0200

    sonata upgrade 9

--- src/AulaLoPati/BlogBundle/Admin/ProjecteAdmin.php
+++ src/AulaLoPati/BlogBundle/Admin/ProjecteAdmin.php
@@ -2,89 +2,104 @@
 
 namespace AulaLoPati\BlogBundle\Admin;
 
-use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
-class ProjecteAdmin extends AbstractAdmin
+class ProjecteAdmin extends AbstractBaseAdmin
 {
 
-	protected function configureFormFields(FormMapper $formMapper)
-	{
-		$formMapper
-        ->with('General')
-		->add('titol', null, array('label' => 'Títol'))
-		->add('resum', 'textarea', array('label' => 'Resum','required'  => true, 'attr'=>(array('style'=>'height:160px; width:600px;'))))
-		->add('descripcio', 'textarea', array('attr' => array('class' => 'tinymce',
-				 'data-theme'=>'simple',
-				'style' => 'width: 600px; height: 400px;'),'label' => 'Descripció'))
-				
-				->add('actiu', null, array('label' => 'Actiu ?','required'  => false))
-		//->add('imgPetitaGris',null,array('required'  => false))
-		//->add('imgPetitaMagenta',null,array('required'  => false))
-		->add('data_publicacio', 'date', array('label' => 'Data publicació', 'widget' => 'single_text', 'format' => 'dd-MM-yyyy'))
-		//->add('data_visible', null, array('label' => 'Data visible ?', 'required'  => false))
-		//->add('data_caducitat','date', array('label' => 'Data caducitat', 'widget' => 'single_text', 'format' => 'dd-MM-yyyy','required'  => false))
-		->add('data_realitzacio', null, array('label' => 'Data realització'))
-		//->add('lloc', null, array('label' => 'Lloc'))
-		//->add('video',null, array('required'=> FALSE))
-		->add('actiu', null, array('label' => 'Actiu ?','required'  => false))
-        ->end()
-		->with('Imatge en miniatura')
-		->add('imagePetita', 'file', array('label' => 'Imatge en miniatura', 'required'=>false))
-		->add('imagePetitaName', null, array('label' => 'Nom', 'required' => false, 'read_only'=>true,))
-		->end()
-		->with('Imatge principal')
-		->add('imageGran1', 'file', array('label' => 'Arxiu', 'required'=>false))
-		->add('imageGran1Name', null, array('label' => 'Nom', 'required'=>false, 'read_only'=>true,))
-		->add('peuImageGran1', null, array('label' => 'Peu imatge', 'required'=>false))
-		
-        ->setHelps(array('titol'=>'Camp únic, no es pot repetir.'))
-        ->setHelps(array('data_realitzacio'=>'Ex: "dia: 19 octubre _ hora: 15:00h _ lloc: Lo Pati"'))
-        ->setHelps(array('resum'=>'Max: 300 caràcters'))
-		->setHelps(array('data_publicacio'=>'Format: dd-MM-yyyy'))
-		->setHelps(array('data_caducitat'=>'Data fins quan sera visible la pàgina -> Automaticament serà Arxiu. Deixar en blanc per no caducar. Format: dd-MM-yyyy '))
-		;
-	}
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General', array('class' => 'col-md-6', 'box_class' => 'box box-success'))
+            ->add('titol', null, array('label' => 'Títol'))
+            ->add('resum', 'textarea', array('label' => 'Resum', 'required' => true, 'attr' => (array('rows' => '5'))))
+            ->add(
+                'descripcio',
+                'textarea',
+                array(
+                    'attr' => array(
+                        'class' => 'tinymce',
+                        'data-theme' => 'simple',
+                        'style' => 'width:100%;height:480px;',
+                    ),
+                    'label' => 'Descripció',
+                )
+            )
+            ->end()
+            ->with('Controls', array('class' => 'col-md-6', 'box_class' => 'box box-success'))
+            ->add('actiu', null, array('label' => 'Actiu', 'required' => false))
+            //->add('imgPetitaGris',null,array('required'  => false))
+            //->add('imgPetitaMagenta',null,array('required'  => false))
+            ->add(
+                'data_publicacio',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'Data publicació',
+                    'format' => 'd/M/y',
+                )
+            )
+            //->add('data_visible', null, array('label' => 'Data visible ?', 'required'  => false))
+            //->add('data_caducitat','date', array('label' => 'Data caducitat', 'widget' => 'single_text', 'format' => 'dd-MM-yyyy','required'  => false))
+            ->add('data_realitzacio', null, array('label' => 'Data realització'))
+            //->add('lloc', null, array('label' => 'Lloc'))
+            //->add('video',null, array('required'=> FALSE))
+            ->end()
+            ->with('Imatge en miniatura', array('class' => 'col-md-6', 'box_class' => 'box box-success'))
+            ->add('imagePetita', 'file', array('label' => 'Imatge en miniatura', 'required' => false, 'help' => $this->getImageHelperFormMapperWithThumbnailSmall()))
+            ->add('imagePetitaName', null, array('label' => 'Nom', 'required' => false, 'read_only' => true,))
+            ->end()
+            ->with('Imatge principal', array('class' => 'col-md-6', 'box_class' => 'box box-success'))
+            ->add('imageGran1', 'file', array('label' => 'Arxiu', 'required' => false, 'help' => $this->getImageHelperFormMapperWithThumbnail()))
+            ->add('imageGran1Name', null, array('label' => 'Nom', 'required' => false, 'read_only' => true,))
+            ->add('peuImageGran1', null, array('label' => 'Peu imatge', 'required' => false))
+            ->setHelps(array('titol' => 'Camp únic, no es pot repetir.'))
+            ->setHelps(array('data_realitzacio' => 'Ex: "dia: 19 octubre _ hora: 15:00h _ lloc: Lo Pati"'))
+            ->setHelps(array('resum' => 'Max: 300 caràcters'))
+            ->setHelps(
+                array('data_caducitat' => 'Data fins quan sera visible la pàgina -> Automaticament serà Arxiu. Deixar en blanc per no caducar. Format: dd-MM-yyyy ')
+            );
+    }
 
-	protected function configureListFields(ListMapper $mapper)
-	{
+    protected function configureListFields(ListMapper $mapper)
+    {
         unset($this->listModes['mosaic']);
-		$mapper
-		->add('id')
-		->addIdentifier('titol', null, array('label' => 'Títol'))
-		->add('actiu', null, array('editable' => true))
-            ->add('_action', 'actions', array(
-                'actions' => array(
-                    //'view' => array(),
-                    'edit' => array(),
-                ),
-                'label' => 'Acció'
-            ))
-		;
-	}
+        $mapper
+            ->add('id')
+            ->addIdentifier('titol', null, array('label' => 'Títol'))
+            ->add('actiu', null, array('editable' => true))
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        //'view' => array(),
+                        'edit' => array(),
+                    ),
+                    'label' => 'Acció',
+                )
+            );
+    }
 
-	protected $datagridValues = array(
-			'_page' => 1,
-			'_sort_order' => 'ASC', // sort direction
-			'_sort_by' => 'titol' // field name
-	);
-	
-	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    protected $datagridValues = array(
+        '_page' => 1,
+        '_sort_order' => 'ASC', // sort direction
+        '_sort_by' => 'titol' // field name
+    );
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
             ->add('titol')
-            ->add('actiu')
-        ;
+            ->add('actiu');
     }
 
     protected function configureRoutes(RouteCollection $collection)
     {
         $collection
             //->remove('create')
-            ->remove('delete')
-        ;
+            ->remove('delete');
     }
 }
